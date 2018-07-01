c4d484bb6a6eada7bdaae1c54fcf7e0c9f4a84c7
keepfool <crmug@outlook.com>
2018-07-01 11:28:11 +0800

    fix: missing config.override.js

---
+++ config-override.js
@@ -0,0 +1,13 @@
+const { injectBabelPlugin } = require('react-app-rewired')
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  // 生产环境不需要hot-reloader
+  if (env === 'production') {
+    return config
+  }
+
+  // 向babel-loader添加react-hot-loader插件
+  config = injectBabelPlugin('react-hot-loader/babel', config)
+  return config
+}
